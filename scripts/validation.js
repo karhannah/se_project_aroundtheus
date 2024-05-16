07397b3ceb64d0291a6f0515e075fd2a4ca428d1
karhan <karinnehannah@gmail.com>
2024-05-15 19:32:07 -0700

    Fix/CSS inconsistencies

--- scripts/validation.js
+++ scripts/validation.js
@@ -67,9 +67,9 @@ const config = {
   formSelector: ".modal__form",
   inputSelector: ".modal__input",
   submitButtonSelector: ".modal__button",
-  inactiveButtonClass: "modal__button_disabled",
-  inputErrorClass: "popup__input_type_error",
-  errorClass: "popup__error_visible",
+  inactiveButtonClass: ".modal__button_disabled",
+  inputErrorClass: ".modal__input_type_error",
+  errorClass: ".modal__error_visible",
 };
 
 enableValidation(config);
