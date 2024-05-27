f6c4e1511cf71de86b14a364c29c5d37f4b29574
karhan <karinnehannah@gmail.com>
2024-05-26 19:43:32 -0700

    project ready for submission, errors won't show

--- components/card.js
+++ components/card.js
@@ -1,27 +1,33 @@
 export default class Card {
-  constructor({ name, link }, cardSelector) {
+  constructor({ name, link }, cardSelector, handleImageClick) {
     this._name = name;
     this._link = link;
     this._cardSelector = cardSelector;
+    this._handleImageClick = handleImageClick;
+  }
+
+  _getTemplate() {
+    const cardElement = document
+      .querySelector(this._cardSelector)
+      .content.querySelector(".card")
+      .cloneNode(true);
+    return cardElement;
   }
 
   _setEventListeners() {
-    this._cardElement
-      .querySelector(".card__like-button")
-      .addEventListener("click", () => {
-        this.handleLikeIcon();
-      });
-    this._cardElement
-      .querySelector(".card__delete-button")
-      .addEventListener("click", () => {
-        this._handleDeleteCard();
-      });
+    this._likeButton.addEventListener("click", () => {
+      this._handleLikeIcon();
+    });
+    this._deleteButton.addEventListener("click", () => {
+      this._handleDeleteCard();
+    });
+    this._cardElement.addEventListener("click", () => {
+      this._handleImageClick(this);
+    });
   }
 
   _handleLikeIcon() {
-    this._cardElement
-      .querySelector(".card__like-button")
-      .classList.toggle(".card__like-button_is-active");
+    this._likeButton.classList.toggle(".card__like-button_is-active");
   }
 
   _handleDeleteCard() {
@@ -31,13 +37,24 @@ export default class Card {
 
   _handleCardPreview() {
     this._cardElement.querySelector(".modal__image-preview");
+    previewImageImageEl.src = this._link;
+    previewImageTextEl.textContent = this._name;
+    previewImageImageEl.alt = this._name;
+    openPopup(previewImageModal);
   }
 
   getView() {
-    this._cardElement = document
-      .querySelector(this._cardSelector)
-      .content.querySelector(".card")
-      .cloneNode(true);
+    this._cardElement = this._getTemplate();
+    this._cardImageElement = this._cardElement.querySelector(".card__image");
+    this._cardTitleElement = this._cardElement.querySelector(".card__title");
+    this._deleteButton = this._cardElement.querySelector(
+      ".card__delete-button"
+    );
+    this._likeButton = this._cardElement.querySelector(".card__like-button");
+    this._cardImageElement.src = this._link;
+    this._cardTitleElement.textContent = this._name;
+    this._cardImageElement.alt = this._name;
     this._setEventListeners();
+    return this._cardElement;
   }
 }
--- components/validation.js
+++ components/validation.js
@@ -1,79 +1,78 @@
-function showInputError(formEl, inputEl, { inputErrorClass, errorClass }) {
-  const errorMessageEl = formEl.querySelector(`#${inputEl.id}-error`);
-  inputEl.classList.add(inputErrorClass);
-  errorMessageEl.textContent = inputEl.validationMessage;
-  errorMessageEl.classList.add(errorClass);
-}
+class FormValidator {
+  constructor(config, formEl) {
+    this._config = config;
+    this._formEl = formEl;
+    this._inputSelector = config.inputSelector;
+    this._submitButtonSelector = config.submitButtonSelector;
+    this._inactiveButtonClass = config.inactiveButtonClass;
+    this._inputErrorClass = config.inputErrorClass;
+    this._errorClass = config.errorClass;
+  }
 
-function hideInputError(formEl, inputEl, { inputErrorClass, errorClass }) {
-  const errorMessageEl = formEl.querySelector(`#${inputEl.id}-error`);
-  inputEl.classList.remove(inputErrorClass);
-  errorMessageEl.textContent = "";
-  errorMessageEl.classList.remove(errorClass);
-}
+  _showInputError(inputEl) {
+    const errorMessageEl = this._formEl.querySelector(`#${inputEl.id}-error`);
+    inputEl.classList.add(this._inputErrorClass);
+    errorMessageEl.textContent = inputEl.validationMessage;
+    errorMessageEl.classList.add(this._errorClass);
+  }
 
-function enableValidation(options) {
-  const formEls = [...document.querySelectorAll(`.${options.formSelector}`)];
-  formEls.forEach((formEl) => {
-    formEl.addEventListener("submit", (e) => {
+  _hideInputError(inputEl) {
+    const errorMessageEl = this._formEl.querySelector(`#${inputEl.id}-error`);
+    inputEl.classList.remove(this._inputErrorClass);
+    errorMessageEl.textContent = "";
+    errorMessageEl.classList.remove(this._errorClass);
+  }
+
+  enableValidation() {
+    this._formEl.addEventListener("submit", (e) => {
       e.preventDefault();
     });
-    setEventListeners(formEl, options);
-  });
-}
-
-function checkInputValidity(formEl, inputEl, config) {
-  if (!inputEl.validity.valid) {
-    showInputError(formEl, inputEl, config);
-  } else {
-    hideInputError(formEl, inputEl, config);
+    this._setEventListeners();
   }
-}
 
-function disableButton(submitButton, inactiveButtonClass) {
-  submitButton.classList.add(inactiveButtonClass);
-  submitButton.disabled = true;
-}
-
-function enableButton(submitButton, inactiveButtonClass) {
-  submitButton.classList.remove(inactiveButtonClass);
-  submitButton.disabled = false;
-}
-
-function hasInvalidInput(inputList) {
-  return inputList.some((input) => !input.validity.valid);
-}
-
-function toggleButtonState(inputList, submitButton, { inactiveButtonClass }) {
-  if (hasInvalidInput(inputList)) {
-    disableButton(submitButton, inactiveButtonClass);
-  } else {
-    enableButton(submitButton, inactiveButtonClass);
+  _checkInputValidity(inputEl) {
+    if (!inputEl.validity.valid) {
+      this._showInputError(inputEl);
+    } else {
+      this._hideInputError(inputEl);
+    }
   }
-}
 
-function setEventListeners(formEl, options) {
-  const { inputSelector, submitButtonSelector } = options;
-  const inputList = [...formEl.querySelectorAll(`.${inputSelector}`)];
-  const submitButton = formEl.querySelector(`.${submitButtonSelector}`);
+  _disableButton() {
+    this._submitButton.classList.add(this._inactiveButtonClass);
+    this._submitButton.disabled = true;
+  }
 
-  inputList.forEach((inputEl) => {
-    inputEl.addEventListener("input", () => {
-      checkInputValidity(formEl, inputEl, options);
-      toggleButtonState(inputList, submitButton, options);
+  _enableButton() {
+    this._submitButton.classList.remove(this._inactiveButtonClass);
+    this._submitButton.disabled = false;
+  }
+
+  _hasInvalidInput(inputList) {
+    return inputList.some((inputEl) => !inputEl.validity.valid);
+  }
+
+  _toggleButtonState() {
+    if (this._hasInvalidInput(this._inputEls)) {
+      this._disableButton();
+    } else {
+      this._enableButton();
+    }
+  }
+
+  _setEventListeners() {
+    this._inputEls = Array.from(
+      this._formEl.querySelectorAll(this._inputSelector)
+    );
+    this._submitButton = this._formEl.querySelector(this._submitButtonSelector);
+
+    this._inputEls.forEach((inputEl) => {
+      inputEl.addEventListener("input", () => {
+        this._checkInputValidity(inputEl);
+        this._toggleButtonState();
+      });
     });
-  });
-
-  toggleButtonState(inputList, submitButton, options);
+  }
 }
 
-const config = {
-  formSelector: "modal__form",
-  inputSelector: "modal__input",
-  submitButtonSelector: "modal__button",
-  inactiveButtonClass: "modal__button_disabled",
-  inputErrorClass: "modal__input_type_error",
-  errorClass: "modal__error_visible",
-};
-
-enableValidation(config);
+export default FormValidator;
