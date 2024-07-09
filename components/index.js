a3f277ef532374a05e97975c451466da9583bd8d
karhan <karinnehannah@gmail.com>
2024-07-09 00:32:05 -0700

    Project 8

--- components/index.js
+++ components/index.js
@@ -6,77 +6,42 @@ import PopupWithForm from "./PopupWithForm.js";
 import PopupWithImage from "./PopupWithImage.js";
 import Section from "./Section.js";
 import UserInfo from "./UserInfo.js";
+import * as constants from "./constants.js";
 
-// Initial cards data
-const initialCards = [
-  {
-    name: "Yosemite Valley",
-    link: "https://practicum-content.s3.us-west-1.amazonaws.com/software-engineer/around-project/yosemite.jpg",
-  },
-  {
-    name: "Lake Louise",
-    link: "https://practicum-content.s3.us-west-1.amazonaws.com/software-engineer/around-project/lake-louise.jpg",
-  },
-  {
-    name: "Bald Mountains",
-    link: "https://practicum-content.s3.us-west-1.amazonaws.com/software-engineer/around-project/bald-mountains.jpg",
-  },
-  {
-    name: "Latemar",
-    link: "https://practicum-content.s3.us-west-1.amazonaws.com/software-engineer/around-project/latemar.jpg",
-  },
-  {
-    name: "Vanoise National Park",
-    link: "https://practicum-content.s3.us-west-1.amazonaws.com/software-engineer/around-project/vanoise.jpg",
-  },
-  {
-    name: "Lago di Braies",
-    link: "https://practicum-content.s3.us-west-1.amazonaws.com/software-engineer/around-project/lago.jpg",
-  },
-];
-
-const section = new Section ({items:initialCards, renderer: addCard}, ".gallery__cards");
-section.renderItems();
-
-// Elements
+const section = new Section ({items:constants.initialCards, renderer: addCard}, ".gallery__cards");
 const cardTemplate = document
-  .querySelector("#card-template")
-  .content.querySelector(".card");
-const profileInfo = new UserInfo("Jacques Cousteau", "Explorer");
-const cardListEl = document.querySelector(".gallery__cards");
-const addNewCardButton = document.querySelector("#profile__add-button");
-const profileTitle = document.querySelector("#profile__title");
-const profileDescription = document.querySelector("#profile__description");
-const profileTitleInput = document.querySelector("#modal__title");
-const profileDescriptionInput = document.querySelector("#modal__description");
-const profileEditButton = document.querySelector("#profile__edit-button");
-const profileEditModal = document.querySelector("#profileEditModal");
-const profileEditModalClose = document.querySelector("#profile__edit-modal-close");
-const profileModalForm = document.querySelector("#profile-form");
+    .querySelector("#card-template")
+    .content.querySelector(".card");
+  const profileInfo = new UserInfo("Jacques Cousteau", "Explorer");
+  const cardListEl = document.querySelector(".gallery__cards");
+  const addNewCardButton = document.querySelector("#profile__add-button");
+  const profileTitle = document.querySelector("#profile__title");
+  const profileDescription = document.querySelector("#profile__description");
+  const profileTitleInput = document.querySelector("#modal__title");
+  const profileDescriptionInput = document.querySelector("#modal__description");
+  const profileEditButton = document.querySelector("#profile__edit-button");
+  const profileEditModal = document.querySelector("#profileEditModal");
+  const profileEditModalClose = document.querySelector("#profile__edit-modal-close");
+  const profileModalForm = document.querySelector("#profile-form");
+
+  const cardModalClose = document.querySelector("#profile__add-modal-close");
+  const cardForm = document.querySelector("#add-form");
 const profileFormModal = new PopupWithForm({popupSelector:"#profileEditModal", handleFormSubmit:handleProfileEditSubmit});
 const cardAddModal = new PopupWithForm ({popupSelector:"#cardModal", handleFormSubmit:handleAddCardSubmit});
-const cardModalClose = document.querySelector("#profile__add-modal-close");
-const cardForm = document.querySelector("#add-form");
+const previewImagePopup = new PopupWithImage("#previewImageModal");
 const cardTitleInput = cardAddModal.modal.querySelector("#modal__new-title");
 const cardImageInput = cardAddModal.modal.querySelector("#modal__new-description");
-const previewImagePopup = new PopupWithImage("#previewImageModal");
 const previewImageImageEl = previewImagePopup.modal.querySelector(".modal__image");
 const closeImageModal = previewImagePopup.modal.querySelector(".modal__close");
-
-// Form settings
-const settings = {
-  inputSelector: ".modal__input",
-  submitButtonSelector: ".modal__button",
-  inactiveButtonClass: "modal__button_disabled",
-  inputErrorClass: "modal__input_type_error",
-  errorClass: "modal__error_visible",
-};
+const addCardFormValidator = new FormValidator(constants.settings, cardForm);
+const profileFormValidator = new FormValidator(constants.settings, profileModalForm);
 
 // Enable form validation
-const addCardFormValidator = new FormValidator(settings, cardForm);
+
+section.renderItems();
+
 addCardFormValidator.enableValidation();
 
-const profileFormValidator = new FormValidator(settings, profileModalForm);
 profileFormValidator.enableValidation();
 
 function addCard(cardData) {
